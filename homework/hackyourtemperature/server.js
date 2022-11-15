a2ff87a13beb7b9d6eb5054e0ce41e1d7dcff5e8
GOZTURK7 <gokhan.89.ozturk@gmail.com>
2022-11-15 22:31:16 +0100

    finished project for homework week1

---
+++ homework/hackyourtemperature/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('hello from backend to frontend!')
+});
+
+app.post('/weather', (req, res)=>{
+    let cityName = req.body.cityName;
+    console.log(cityName);
+    res.send(cityName);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
