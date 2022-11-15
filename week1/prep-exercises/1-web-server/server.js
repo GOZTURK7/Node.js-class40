2de2635a2aed4747d6c5929a0700ff9d156dbf0f
GOZTURK7 <gokhan.89.ozturk@gmail.com>
2022-11-15 20:06:23 +0100

    1-web-server

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -2,13 +2,47 @@
  * Exercise 3: Create an HTTP web server
  */
 
-const http = require('http');
+ const http = require('http');
+ const fs = require('fs');
+ 
+ //create a server
+ const server = http.createServer(function (req, res) {
+   // YOUR CODE GOES IN HERE
+	 
+	 if(req.url==='/'){
+		 
+		 fs.readFile(
+			'./index.html', 
+			(err, data) => {
+				if(err) throw err;
+				res.writeHead(200, {'content-type': 'text/html'});
+				res.end(data);
+				return
 
-//create a server
-let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
-});
+		 })
+		}else if(req.url==='/index.js'){
+			fs.readFile(
+				'./index.js', 
+				(err, data) => {
+					if(err) throw err;
+					res.writeHead(200, {'content-type': 'application/json'});
+					res.end(data)
+					return
+			})
+		}else if(req.url === '/style.css'){
+			fs.readFile('./style.css', (err, data) => {
+				res.writeHead(200, {'content-type': 'text/css'});
+				res.end(data);
+				return
+			})
+		}
+	
+	});
+ 
+	 // and after use it 
+	 // res.write('./index.html' ); // Sends a response back to the client
+	 // res.end(); // Ends the response
 
-server.listen(3000); // The server starts to listen on port 3000
+ 
+ server.listen(3000); // The server starts to listen on port 3000
+ 
