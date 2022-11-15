5cc1d3ee9f0b570e3a6d084902c5ba264a10e6b7
GOZTURK7 <gokhan.89.ozturk@gmail.com>
2022-11-15 23:23:15 +0100

    Updated Hackyourtemprature homework-1

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+import express from 'express';
 const app = express()
 const port = 3000
 
@@ -11,8 +11,8 @@ app.get('/', (req, res) => {
 app.post('/weather', (req, res)=>{
     let cityName = req.body.cityName;
     console.log(cityName);
-    res.send(cityName);
-    
+    res.json({cityName});
+
 })
 
 app.listen(port, () => {
